020165fb5e367a662ccc584502bb0e4f0424a9ad
honoryk <118126185+honoryk@users.noreply.github.com>
2025-10-20 11:41:30 +0200

    Lab 3 zad 3

--- Lab3_Grzebyk_21236/index.js
+++ Lab3_Grzebyk_21236/index.js
@@ -1,34 +1,25 @@
-console.log('Lab 3 zad 2');
+console.log('Lab 3 zad 3');
 
-// 1
-const math1 = require('./math1');
+const http = require('http');
+var url = require('url');
+const modul = require('./modulstrony');
 
-// 2
-const Math2Module = require('./math2');
-const math2 = new Math2Module(5,6);
+const requestListener = function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'}); 
+    //res.end(modul.glowna());
+    
+    switch(req.url){
+        case '/about':
+            res.end(modul.about());
+            break;
+        case '/contact':
+            res.end(modul.contact());
+            break;
+        default:
+            res.end(modul.glowna());
+            break;
+    }
+    
+}
 
-
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question("Wczytaj liczbe x: ", function(x){
-    rl.question("Wczytaj liczbe y: ", function(y){
-        console.log('Sposob 1: ');
-        console.log('Suma =' + math1.suma(x,y));
-        console.log('Roznica = ' + math1.roznica(x,y));
-        console.log('===============');
-
-        console.log('Sposob 2: ');
-        console.log('Iloczyn = ' + math2.iloczyn());
-        console.log('Iloraz = ' + math2.iloraz());
-        rl.close();
-    })
-})
-
-rl.on("close", function(){
-    console.log("Koniec");
-    process.exit(0);
-});
+const server = http.createServer(requestListener).listen(8000, "127.0.0.1");
---
+++ Lab3_Grzebyk_21236/modulstrony.js
@@ -0,0 +1,11 @@
+exports.glowna = function(){
+    return '<html><head></head><body><h1>Glowna</h1></body></html>';
+}
+
+exports.about = function(){
+    return '<html><head></head><body><h1>About</h1></body></html>';
+}
+
+exports.contact = function(){
+    return '<html><head></head><body><h1>Contact</h1></body></html>';
+}
