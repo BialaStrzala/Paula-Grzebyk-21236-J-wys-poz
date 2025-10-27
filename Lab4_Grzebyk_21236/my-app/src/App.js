55e926ea721f3970396f2cc844c1269c2babe7ef
honoryk <118126185+honoryk@users.noreply.github.com>
2025-10-27 10:46:29 +0100

    Lab 4 cw 2

--- Lab4_Grzebyk_21236/my-app/src/App.js
+++ Lab4_Grzebyk_21236/my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from './Hello';
+import HelloWithProps from './HelloWithProps';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Hello/>
+        <HelloWithProps name="Paula"/>
+        <HelloWithProps name="Ewa"/>
+        <HelloWithProps name="Adam"/>
       </header>
     </div>
   );
