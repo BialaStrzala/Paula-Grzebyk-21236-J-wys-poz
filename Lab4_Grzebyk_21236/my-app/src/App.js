54719df4973e1be50ff79cd5b5cf65e8b9e491dc
honoryk <118126185+honoryk@users.noreply.github.com>
2025-10-27 10:54:25 +0100

    Lab 4 cw 5

--- Lab4_Grzebyk_21236/my-app/src/App.js
+++ Lab4_Grzebyk_21236/my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import InputTracker from './InputTracker';
+import LoginStatus from './LoginStatus';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <InputTracker/>
+        <LoginStatus isLoggedIn={true}/>
+        <LoginStatus isLoggedIn={false}/>
       </header>
     </div>
   );
