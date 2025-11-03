8865e80210cd8c320121d9c7510273a285332980
honoryk <118126185+honoryk@users.noreply.github.com>
2025-11-03 10:13:08 +0100

    Lab 5 cw 1

---
+++ Lab5_Grzebyk_21236/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ToggleDetails from './ToggleDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ToggleDetails/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
