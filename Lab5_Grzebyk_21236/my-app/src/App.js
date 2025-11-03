7ad21e33fe677786bdf636909ff058853a7b30fb
honoryk <118126185+honoryk@users.noreply.github.com>
2025-11-03 10:15:26 +0100

    Lab 5 cw 2

--- Lab5_Grzebyk_21236/my-app/src/App.js
+++ Lab5_Grzebyk_21236/my-app/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ToggleDetails from './ToggleDetails';
+import ScoreDisplay from './ScoreDisplay';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ToggleDetails/>
+        <ScoreDisplay score={10}/>
+        <ScoreDisplay score={50}/>
+        <ScoreDisplay score={80}/>
       </header>
     </div>
   );
