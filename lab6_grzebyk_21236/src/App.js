255fa1babd0d752ea646d6a22954c307681cfb2f
honoryk <118126185+honoryk@users.noreply.github.com>
2025-11-17 10:33:35 +0100

    lab 6 cw 3

--- lab6_grzebyk_21236/src/App.js
+++ lab6_grzebyk_21236/src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import ClickCounter from './ClickCounter';
 import PrimeCalculator from './PrimeCalculator';
+import FormReducer from './FormReducer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ClickCounter/>
-        <PrimeCalculator/>
+        <FormReducer/>
       </header>
     </div>
   );
